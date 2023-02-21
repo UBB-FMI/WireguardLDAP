522c460114935381b45a6383c0060c029502db15
KiralyCraft <alexucu@gmail.com>
2023-02-22 00:41:22 +0200

    Fix JSON Builder

---
+++ jsonBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+	if ($_SERVER['REQUEST_METHOD'] === 'GET') { exit; }
+
+	function buildJSONResponse($theMessage,$theCode,$data = NULL)
+	{
+		if ($data !== NULL)
+		{
+			$toReturn = array("msg"=>$theMessage,"code"=>$theCode,"data"=>$data);
+		}
+		else
+		{
+			$toReturn = array("msg"=>$theMessage,"code"=>$theCode);
+		}
+		return json_encode($toReturn);
+	}
+?>
