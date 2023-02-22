222d662e6f979151a562b9db9cb67b3ced6c49ad
KiralyCraft <alexucu@gmail.com>
2023-02-23 00:57:30 +0200

    Take AllowedIPs from the standard definitions

--- standardDefinitions.php
+++ standardDefinitions.php
@@ -14,6 +14,17 @@
 		"maxIP"=> "10.0.31.254",
 		"netMask"=>"20",
 		"pubKey"=>"ZBaK+E4YDFGGzWlG5cDYt/eFRQ6ajL7env4HYC7CK0E=",
-		"port"=>"51820");
+		"port"=>"51820",
+		"allowedIPs"=>"0.0.0.0/0"
+	);
+	$standardServerParameters[1] = array(
+		"interface"=>"unprivileged",
+		"baseIP"=>"10.0.32.2",
+		"maxIP"=> "10.0.47.254",
+		"netMask"=>"20",
+		"pubKey"=>"KkxqXijYvoAl96xgVUOwl1J8yz9f48z1/fZH0HucnkU=",
+		"port"=>"51821",
+		"allowedIPs"=>"172.30.0.0/16"
+	);
 
 ?>
